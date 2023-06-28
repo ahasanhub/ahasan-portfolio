b8c1e0621587a9f11261ec4e8f4b22035d181a1e
Ahasan <shobuz.cse03@gmail.com>
2023-06-28 11:35:46 +0600

    initial commit

---
+++ src/app/blog/page.js
@@ -0,0 +1,61 @@
+export default function Blog() {
+    return (
+        <main id="main">
+            <section id="about" class="about">
+                <div class="container">
+
+                    <div class="section-title">
+                        <h2>About</h2>
+                        <p>Highly skilled and innovative Software Engineer with 14+ years of expertise in challenging environments.
+                            Demonstrated
+                            expertise in delivering creative and performance-driven solutions with a strong focus on development. Strong
+                            project
+                            management and team leadership skills with a customer-centric approach. Proven track record as a Dynamics
+                            365
+                            NAV/Business Central developer, specializing in the power sector industry, with over 6 years of experience
+                            planning,
+                            developing, and implementing Microsoft Dynamics NAV and Business Central solutions.</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                            <h3>Full-Stack Software Engineer |Web, Moble, Dynamics 365 BC/NAV</h3>
+                            <p class="fst-italic">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                                dolore
+                                magna aliqua.
+                            </p>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <ul>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6">
+                                    <ul>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span>
+                                        </li>
+                                        <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <p>
+                                Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et
+                                ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                                Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+                                Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </main>
+    );
+}
+
