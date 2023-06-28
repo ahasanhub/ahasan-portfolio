45e02875dc27369249e8a072fa306602ad85e50b
Ahasan <shobuz.cse03@gmail.com>
2023-06-28 15:05:52 +0600

    fixed background image on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,33 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import bg from '/public/hero-bg.jpg'
-import profileimg from '/public/img/profile-img.jpg'
+import bg from '/public/img/hero-bg.jpg'
+import profilePic from '/public/img/profile-img.jpg'
 
 export default function Home() {
+
   return (
     <>
-      <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
-        <div class="hero-container" data-aos="fade-in">
+      <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+        <Image
+          src={bg}
+          fill
+          sizes="100vw"
+          style={{ objectFit: "cover" }}
+          quality={100}
+        ></Image>
+        <div className="hero-container" data-aos="fade-in">
           <h1>Ahasan Habib</h1>
-          <p>I'm Proficient at<span class="typed"
+          <p>I'm Proficient at<span className="typed"
             data-typed-items="Full-Stack Developer, Freelancer, Software Architect, Dynamics 365 Developer"></span></p>
         </div>
       </section>
       <main id="main">
 
 
-        <section id="about" class="about">
-          <div class="container">
+        <section id="about" className="about">
+          <div className="container">
 
-            <div class="section-title">
+            <div className="section-title">
               <h2>About</h2>
               <p>Highly skilled and innovative Software Engineer with 14+ years of expertise in challenging environments.
                 Demonstrated
@@ -32,33 +40,33 @@ export default function Home() {
                 developing, and implementing Microsoft Dynamics NAV and Business Central solutions.</p>
             </div>
 
-            <div class="row">
-              <div class="col-lg-4" data-aos="fade-right">
-                <Image src={profileimg} className='img-fluid' alt=''></Image>
+            <div className="row">
+              <div className="col-lg-4" data-aos="fade-right">
+                <Image src={profilePic} className='img-fluid' alt=''></Image>
               </div>
-              <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+              <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
                 <h3>Full-Stack Software Engineer |Web, Moble, Dynamics 365 BC/NAV</h3>
-                <p class="fst-italic">
+                <p className="fst-italic">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                   dolore
                   magna aliqua.
                 </p>
-                <div class="row">
-                  <div class="col-lg-6">
+                <div className="row">
+                  <div className="col-lg-6">
                     <ul>
-                      <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                      <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                      <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                      <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
                     </ul>
                   </div>
-                  <div class="col-lg-6">
+                  <div className="col-lg-6">
                     <ul>
-                      <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                      <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                      <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span>
+                      <li><i className="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span>
                       </li>
-                      <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                      <li><i className="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
                     </ul>
                   </div>
                 </div>
@@ -75,50 +83,50 @@ export default function Home() {
         </section>
 
 
-        <section id="facts" class="facts">
-          <div class="container">
+        <section id="facts" className="facts">
+          <div className="container">
 
-            <div class="section-title">
+            <div className="section-title">
               <h2>Facts</h2>
               <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
                 consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
                 in iste officiis commodi quidem hic quas.</p>
             </div>
 
-            <div class="row no-gutters">
+            <div className="row no-gutters">
 
-              <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
-                <div class="count-box">
-                  <i class="bi bi-emoji-smile"></i>
+              <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
+                <div className="count-box">
+                  <i className="bi bi-emoji-smile"></i>
                   <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1"
-                    class="purecounter"></span>
+                    className="purecounter"></span>
                   <p><strong>Happy Clients</strong> consequuntur quae</p>
                 </div>
               </div>
 
-              <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
-                <div class="count-box">
-                  <i class="bi bi-journal-richtext"></i>
+              <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
+                <div className="count-box">
+                  <i className="bi bi-journal-richtext"></i>
                   <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1"
-                    class="purecounter"></span>
+                    className="purecounter"></span>
                   <p><strong>Projects</strong> adipisci atque cum quia aut</p>
                 </div>
               </div>
 
-              <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
-                <div class="count-box">
-                  <i class="bi bi-headset"></i>
+              <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
+                <div className="count-box">
+                  <i className="bi bi-headset"></i>
                   <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1"
-                    class="purecounter"></span>
+                    className="purecounter"></span>
                   <p><strong>Hours Of Support</strong> aut commodi quaerat</p>
                 </div>
               </div>
 
-              <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
-                <div class="count-box">
-                  <i class="bi bi-people"></i>
+              <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
+                <div className="count-box">
+                  <i className="bi bi-people"></i>
                   <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1"
-                    class="purecounter"></span>
+                    className="purecounter"></span>
                   <p><strong>Hard Workers</strong> rerum asperiores dolor</p>
                 </div>
               </div>
@@ -129,32 +137,32 @@ export default function Home() {
         </section>
 
 
-        <section id="skills" class="skills section-bg">
-          <div class="container">
+        <section id="skills" className="skills section-bg">
+          <div className="container">
 
-            <div class="section-title">
+            <div className="section-title">
               <h2>Skills</h2>
               <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
                 consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
                 in iste officiis commodi quidem hic quas.</p>
             </div>
 
-            <div class="row skills-content">
+            <div className="row skills-content">
 
-              <div class="col-lg-6" data-aos="fade-up">
+              <div className="col-lg-6" data-aos="fade-up">
 
-                <div class="progress">
-                  <span class="skill">HTML <i class="val">100%</i></span>
-                  <div class="progress-bar-wrap">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress">
+                  <span className="skill">HTML <i class="val">100%</i></span>
+                  <div className="progress-bar-wrap">
+                    <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
                     </div>
                   </div>
                 </div>
 
-                <div class="progress">
-                  <span class="skill">CSS <i class="val">90%</i></span>
-                  <div class="progress-bar-wrap">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress">
+                  <span className="skill">CSS <i class="val">90%</i></span>
+                  <div className="progress-bar-wrap">
+                    <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
                     </div>
                   </div>
                 </div>
@@ -338,7 +346,7 @@ export default function Home() {
 
 
 
-      </main>
+      </main >
 
 
 
