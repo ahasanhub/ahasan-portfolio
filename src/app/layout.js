15f30efcbe0540f2d1019e5c0e8b1b7c1d7b601b
Ahasan <shobuz.cse03@gmail.com>
2023-07-09 21:31:49 +0600

    added sharp package

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,15 +14,13 @@ export const metadata = {
   title: 'Ahasan',
   description: 'Ahasan Portfolio',
 }
-function handleClick() {
-  alert('Hello');
-}
+
 export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
-        <i className="bi bi-list mobile-nav-toggle d-xl-none"></i>
+        {/* <i className="bi bi-list mobile-nav-toggle d-xl-none"></i> */}
         <SideBar></SideBar>
         <main id="main">{children}</main>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -152,38 +152,38 @@ export default function Home() {
                 </div>
               </div>
 
-              <div class="progress">
-                <span class="skill">JavaScript <i class="val">75%</i></span>
-                <div class="progress-bar-wrap">
-                  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+              <div className="progress">
+                <span className="skill">JavaScript <i className="val">75%</i></span>
+                <div className="progress-bar-wrap">
+                  <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
                   </div>
                 </div>
               </div>
 
             </div>
 
-            <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
 
-              <div class="progress">
-                <span class="skill">PHP <i class="val">80%</i></span>
-                <div class="progress-bar-wrap">
-                  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+              <div className="progress">
+                <span className="skill">PHP <i className="val">80%</i></span>
+                <div className="progress-bar-wrap">
+                  <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
                   </div>
                 </div>
               </div>
 
-              <div class="progress">
-                <span class="skill">WordPress/CMS <i class="val">90%</i></span>
-                <div class="progress-bar-wrap">
-                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+              <div className="progress">
+                <span className="skill">WordPress/CMS <i className="val">90%</i></span>
+                <div className="progress-bar-wrap">
+                  <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
                   </div>
                 </div>
               </div>
 
-              <div class="progress">
-                <span class="skill">Photoshop <i class="val">55%</i></span>
-                <div class="progress-bar-wrap">
-                  <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+              <div className="progress">
+                <span className="skill">Photoshop <i className="val">55%</i></span>
+                <div className="progress-bar-wrap">
+                  <div className="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
                   </div>
                 </div>
               </div>
@@ -196,20 +196,20 @@ export default function Home() {
       </section>
 
 
-      <section id="resume" class="resume">
-        <div class="container">
+      <section id="resume" className="resume">
+        <div className="container">
 
-          <div class="section-title">
+          <div className="section-title">
             <h2>Resume</h2>
             <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
               consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
               in iste officiis commodi quidem hic quas.</p>
           </div>
 
-          <div class="row">
-            <div class="col-lg-6" data-aos="fade-up">
-              <h3 class="resume-title">Sumary</h3>
-              <div class="resume-item pb-0">
+          <div className="row">
+            <div className="col-lg-6" data-aos="fade-up">
+              <h3 className="resume-title">Sumary</h3>
+              <div className="resume-item pb-0">
                 <h4>Alex Smith</h4>
                 <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and
                   developing user-centered digital/print marketing material from initial concept to final, polished
@@ -221,15 +221,15 @@ export default function Home() {
                 </ul>
               </div>
 
-              <h3 class="resume-title">Education</h3>
-              <div class="resume-item">
+              <h3 className="resume-title">Education</h3>
+              <div className="resume-item">
                 <h4>Master of Fine Arts &amp; Graphic Design</h4>
                 <h5>2015 - 2016</h5>
                 <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
                 <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui
                   ut dignissimos deleniti nerada porti sand markend</p>
               </div>
-              <div class="resume-item">
+              <div className="resume-item">
                 <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
                 <h5>2010 - 2014</h5>
                 <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
@@ -238,9 +238,9 @@ export default function Home() {
                   dila</p>
               </div>
             </div>
-            <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-              <h3 class="resume-title">Professional Experience</h3>
-              <div class="resume-item">
+            <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+              <h3 className="resume-title">Professional Experience</h3>
+              <div className="resume-item">
                 <h4>Senior graphic design specialist</h4>
                 <h5>2019 - Present</h5>
                 <p><em>Experion, New York, NY </em></p>
@@ -254,7 +254,7 @@ export default function Home() {
                   <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
                 </ul>
               </div>
-              <div class="resume-item">
+              <div className="resume-item">
                 <h4>Graphic design specialist</h4>
                 <h5>2017 - 2018</h5>
                 <p><em>Stepping Stone Advertising, New York, NY</em></p>
@@ -275,51 +275,51 @@ export default function Home() {
 
 
 
-      <section id="services" class="services">
-        <div class="container">
+      <section id="services" className="services">
+        <div className="container">
 
-          <div class="section-title">
+          <div className="section-title">
             <h2>Services</h2>
             <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
               consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
               in iste officiis commodi quidem hic quas.</p>
           </div>
 
-          <div class="row">
-            <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
-              <div class="icon"><i class="bi bi-briefcase"></i></div>
-              <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+          <div className="row">
+            <div className="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+              <div className="icon"><i className="bi bi-briefcase"></i></div>
+              <h4 className="title"><a href="">Lorem Ipsum</a></h4>
+              <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
                 occaecati cupiditate non provident</p>
             </div>
-            <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-              <div class="icon"><i class="bi bi-card-checklist"></i></div>
-              <h4 class="title"><a href="">Dolor Sitema</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            <div className="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div className="icon"><i className="bi bi-card-checklist"></i></div>
+              <h4 className="title"><a href="">Dolor Sitema</a></h4>
+              <p className="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat tarad limino ata</p>
             </div>
-            <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-              <div class="icon"><i class="bi bi-bar-chart"></i></div>
-              <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            <div className="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div className="icon"><i className="bi bi-bar-chart"></i></div>
+              <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
+              <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur</p>
             </div>
-            <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-              <div class="icon"><i class="bi bi-binoculars"></i></div>
-              <h4 class="title"><a href="">Magni Dolores</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            <div className="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+              <div className="icon"><i className="bi bi-binoculars"></i></div>
+              <h4 className="title"><a href="">Magni Dolores</a></h4>
+              <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                 mollit anim id est laborum</p>
             </div>
-            <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-              <div class="icon"><i class="bi bi-brightness-high"></i></div>
-              <h4 class="title"><a href="">Nemo Enim</a></h4>
-              <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+            <div className="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+              <div className="icon"><i className="bi bi-brightness-high"></i></div>
+              <h4 className="title"><a href="">Nemo Enim</a></h4>
+              <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
                 voluptatum deleniti atque</p>
             </div>
-            <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-              <div class="icon"><i class="bi bi-calendar4-week"></i></div>
-              <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-              <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum
+            <div className="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
+              <div className="icon"><i className="bi bi-calendar4-week"></i></div>
+              <h4 className="title"><a href="">Eiusmod Tempor</a></h4>
+              <p className="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum
                 soluta nobis est eligendi</p>
             </div>
           </div>
@@ -335,8 +335,8 @@ export default function Home() {
 
 
 
-      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-        class="bi bi-arrow-up-short"></i></a>
+      <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i
+        className="bi bi-arrow-up-short"></i></a>
     </>
   )
 }
