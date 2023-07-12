75b6c1c2dbac54bb631c7e3a4c1689d18c43730f
Ahasan <shobuz.cse03@gmail.com>
2023-07-13 01:46:47 +0600

    fixed some layout

---
+++ src/components/HeroDetailSection.js
@@ -0,0 +1,91 @@
+import Image from 'next/image'
+import fullStackDevelopment from '/public/img/full-stack-development.png'
+
+const HeroDetailSection = () => {
+    return (
+        <section id="about" className="about">
+            <div className="container">
+
+                <div className="section-title">
+                    <h2>What I do?</h2>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6" data-aos="fade-right">
+                        <Image src={fullStackDevelopment} className='img-fluid' alt='profile picture'></Image>
+                    </div>
+                    <div className="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+                        <h3>Full Stack Development</h3>
+                        <p className="fst-italic">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore
+                            magna aliqua.
+                        </p>
+                        <ul>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                        </ul>
+                        <p>
+                            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et
+                            ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+                            Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6" data-aos="fade-right">
+                        <Image src={fullStackDevelopment} className='img-fluid' alt='profile picture'></Image>
+                    </div>
+                    <div className="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+                        <h3>Dynamics 365 BC/NAV Development</h3>
+                        <p className="fst-italic">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore
+                            magna aliqua.
+                        </p>
+                        <ul>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                        </ul>
+                        <p>
+                            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et
+                            ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+                            Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6" data-aos="fade-right">
+                        <Image src={fullStackDevelopment} className='img-fluid' alt='profile picture'></Image>
+                    </div>
+                    <div className="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+                        <h3>Cloud Infra-Architecture</h3>
+                        <p className="fst-italic">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore
+                            magna aliqua.
+                        </p>
+                        <ul>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                            <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                        </ul>
+                        <p>
+                            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et
+                            ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+                            Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </section>);
+}
+export default HeroDetailSection;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,38 +4,39 @@ import bg from '/public/img/hero-bg.jpg'
 import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
-    return (<section id="hero" className="d-flex flex-column justify-content-left align-items-left">
-        <Image
-            src={bg}
-            fill
-            sizes="100vw"
-            style={{ objectFit: "cover" }}
-            quality={100}
-        ></Image>
-        <div className='hero-container' >
-            <h1>Ahasan Habib</h1>
-            <p><strong>I'm </strong><TypeAnimation
-                sequence={[
-                    'Full-Stack Software Developer',
-                    2000,
-                    'Dynamics 365 NAV/BC Developer',
-                    2000,
-                    'Software  Generalist',
-                    2000,
-                    'Software Architect',
-                    2000,
-                    'Team Lead',
-                    2000,
-                ]}
-                wrapper="span"
-                cursor={true}
-                repeat={Infinity}
-                style={{ fontSize: '1em', display: 'inline-block' }}
-            />
-            </p>
+    return (
+        <section id="hero" className="d-flex flex-column justify-content-left align-items-left">
+            <Image
+                src={bg}
+                fill
+                sizes="100vw"
+                style={{ objectFit: "cover" }}
+                quality={100}
+            ></Image>
+            <div className='hero-container' >
+                <h1>Ahasan Habib</h1>
+                <p><strong>I'm </strong><TypeAnimation
+                    sequence={[
+                        'Full-Stack Software Developer',
+                        2000,
+                        'Dynamics 365 NAV/BC Developer',
+                        2000,
+                        'Software  Generalist',
+                        2000,
+                        'Software Architect',
+                        2000,
+                        'Team Lead',
+                        2000,
+                    ]}
+                    wrapper="span"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{ fontSize: '1em', display: 'inline-block' }}
+                />
+                </p>
 
-        </div>
-    </section>);
+            </div>
+        </section>);
 }
 
 export default HeroSection;
