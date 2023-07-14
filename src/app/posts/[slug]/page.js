354658ffe13730c2e7fdb84d991084646ea80490
Ahasan <shobuz.cse03@gmail.com>
2023-07-15 02:09:47 +0600

    Add blog post avatar

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import Markdown from 'markdown-to-jsx';
 import matter from 'gray-matter';
 import getPostMetadata from '@/components/getPostMetadata';
+import Image from 'next/image'
+import profilePic from '/public/img/profile-img.jpg'
+
 
 const getPost = (slug) => {
     const folder = path.join(process.cwd(), 'src/posts/');
@@ -20,8 +23,20 @@ const PostPage = (props) => {
     const slug = props.params.slug;
     const post = getPost(slug);
     return (
-        <div className='mx-6 mt-6'>
+        <div className='mx-12 mt-6'>
             <h3 style={{ fontSize: 40, fontWeight: 'bold' }}>{post.data.title}</h3>
+            <div className="relative mt-8 flex items-center gap-x-4">
+                <Image src={profilePic} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href="#">
+                      <span className="absolute inset-0" />
+                      Ahasan Habib
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.data.date}</p>
+                </div>
+              </div>
             <article className="prose lg:prose-xl">
                 <Markdown>{post.content}</Markdown>
             </article>
