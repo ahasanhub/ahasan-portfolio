eccf912b743d407d48d5e208270c4cb66a2c0f27
Ahasan <shobuz.cse03@gmail.com>
2023-07-17 22:54:07 +0600

    ss

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -3,6 +3,7 @@ import Image  from "next/image";
 import bp from "/public/images/thumbnail/npm-commands.png"
 import profilePic from '/public/img/profile-img.jpg'
 import { useRouter } from 'next/navigation'
+import Link from "next/link";
 const PostPreview = (props) => {
     const router = useRouter();
     return (
@@ -13,7 +14,7 @@ const PostPreview = (props) => {
     //     <p className="text-sm text-slate-400">{props.date}</p>
     //     <p className="text-slate-400">{props.subtitle}</p>
     // </div>
-    <div class="grid lg:grid-cols-2 border rounded-lg gap-4 h-auto p-4">
+    <div class="grid lg:grid-cols-2 border rounded-lg gap-4 h-auto p-4 shadow-md">
                  <div class="max-w-2xl  h-auto  flex items-center relative ">
                      <Image src={bp} full quality={100} alt="placeholder img"/>
                      <div class="bg-green-400  w-24 pt-1  h-8 text-gray-50 font-semibold text-center absolute top-0">
@@ -21,7 +22,10 @@ const PostPreview = (props) => {
                      </div>
                  </div>
                  <div class="">
-                     <h3 class="text-2xl font-bold text-gray-800 mb-2 cursor-pointer ">{props.title}</h3>
+                    <Link href={`/posts/${props.slug}`}>
+                    <h3 class="text-2xl font-bold text-gray-800 mb-2 ">{props.title}</h3>
+                    </Link>
+                     
                      <div class="flex gap-2 mb-2">
                          {/* <div class="w-10 h-10 rounded-full bg-gray-300"></div> */}
                          <Image className="h-9 w-9 rounded-full border border-slate-300" src={profilePic} alt="" />  
